3b96e48330dfd5e1088de4f7c87d7713499b5134
shweta1234567 <30323770+shweta1234567@users.noreply.github.com>
2017-09-25 03:44:01 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,6 +11,15 @@ app.get('/', function (req, res) {
 app.get('/article-one',function(req,res){
     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
+app.get('/article-two',function(req,res){
+    //res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+    res.send("Article two requested and will served here.");
+});
+app.get('/article-three',function(req,res){
+    //res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+    res.send("Article three requested and will served here.");
+    
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
