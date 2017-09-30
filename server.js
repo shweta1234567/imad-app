64389c1d120c70db914eed3e0acb936118a01432
shweta1234567 <30323770+shweta1234567@users.noreply.github.com>
2017-09-30 01:47:55 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/', function (req, res) {
 app.get('/test-db',function(req,res){
     pool.query('SELECT * FROM test',function(err,result){
         if(err){
-            res.status(500).send(err.to.String());
+            res.status(500).send(err.toString());
         }
         else{
             res.send(JSON.stringify(result));
